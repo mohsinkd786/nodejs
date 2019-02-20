2f0f7cddcb846af68e6d75bda1c5835c1253434b
Mohsin K <mohsinkd786@gmail.com>
2019-02-20 11:20:02 +0530

    Mongo updated & delete

--- employee-service-mongo/db/dao.js
+++ employee-service-mongo/db/dao.js
@@ -12,20 +12,59 @@ const getEmployees = (callback)=>{
         // Step 3: fetch all documents from the collections
         conn.db(_db).collection('employees').find({},{fields:{ _id:0}}).toArray((err,emps)=>{
             // callback function to handle async flow 
-            callback(err,emps)
+            callback(err,{
+                employees : emps
+            })
         })
+        // close the connection
+        conn.close()
     })
 }
 // add a new employee
-const addEmployee  = (emp)=>{
+const addEmployee  = (emp,callback)=>{
     MongoClient.connect(_url,(err,conn)=>{
         conn.db(_db).collection('employees').insertOne(emp,(err)=>{
-            console.log('Employee Added')
+            callback(err,{
+                message : 'Employee Added'
+            })
         })
+        // close the connection
+        conn.close()
     })
 }
-
+// update an employee
+const updateEmployee = (emp,callback)=>{
+    const id = emp._id
+    delete emp._id
+    console.log(emp)
+    MongoClient.connect(_url,(err,conn)=>{
+        conn.db(_db).collection('employees').updateOne({
+            _id: id
+        },{
+            $set: emp
+            
+        },(err,data)=>{
+            callback(err,{
+                message : 'Employee Updated'
+            })
+        })
+        // close the connection
+        conn.close()
+    })
+}
+// delete an employee with _id
+const deleteById = (eId,callback)=>{
+    MongoClient.connect(_url,(err,conn)=>{
+        conn.db(_db).collection('employees').deleteOne({ _id: eId},(err)=>{
+            callback(err)
+        })
+        // close the connection
+        conn.close()
+    })
+}
 module.exports={
     _all : getEmployees,
-    _add : addEmployee
+    _add : addEmployee,
+    _updateById : updateEmployee,
+    _delById : deleteById
 }
--- employee-service-mongo/routes.js
+++ employee-service-mongo/routes.js
@@ -4,34 +4,56 @@ const dao = require('./db/dao')
 
 // get all employees
 routes.get('/employees',(rq,rs)=>{
-    dao._all((err,emps)=>{
+    dao._all((err,data)=>{
         if(err){
             rs.status(500).send({
                 error: "Unable to load data"
             })
         }else{
-            rs.status(200).send({
-                employees : emps
-            })
+            rs.status(200).send(data)
         }
     })
 })
 
 // add a new employee
 routes.post('/employees/add',(rq,rs)=>{
-    rs.send({
-        data: dao._add(rq.body)
+    dao._add(rq.body,(err,data)=>{
+        if(err){
+            rs.status(500).send({
+                error: "Unable to process the request"
+            })
+        }else{
+            rs.status(201).send(data)
+        }
     })
 })
 
 // edit an employee by id
-routes.put('/employees/edit/:id',(rq,rs)=>{
-
+routes.put('/employees/edit',(rq,rs)=>{
+    dao._updateById(rq.body,(err,data)=>{
+        if(err){
+            rs.status(500).send({
+                error: "Unable to process the request"
+            })
+        }else{
+            rs.status(200).send(data)
+        }
+    })
 })
 
 // delete an employee
 routes.delete('/employees/delete/:id',(rq,rs)=>{
-
+    dao._delById(parseInt(rq.params.id),(err)=>{
+        if(err){
+            rs.status(500).send({
+                error: "Unable to process the request"
+            })
+        }else{
+            rs.status(200).send({
+                message : "Employee Deleted"
+            })
+        }
+    })
 })
 
 // find all employees for a project
