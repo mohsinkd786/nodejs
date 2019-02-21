8bbe0f73023e3da3a307988135745ed9e2d159c3
Mohsin K <mohsinkd786@gmail.com>
2019-02-21 13:25:55 +0530

    Add completion done for test

--- calculator-express-jasmine-istanbul/spec/serverSpec.js
+++ calculator-express-jasmine-istanbul/spec/serverSpec.js
@@ -2,19 +2,22 @@ const request = require('request')
 
 // Calculator Express Service Test Suite
 describe('Calculator Service ',()=>{
-    it('Status API success response code(200)',()=>{
+    it('Status API success response code(200)',(done)=>{
         request.get('http://localhost:6631/calculator/status',(err,rs,body)=>{
         expect(rs.statusCode).toBe(200)
+        done()
         })
     })
-    it('Status API response',()=>{
+    it('Status API response',(done)=>{
         request.get('http://localhost:6631/calculator/status',(err,rs,body)=>{
         expect(body).toBe('Hello')
+        done()
         })
     })
-    it('Addition of 5 & 3',()=>{
+    it('Addition of 5 & 3',(done)=>{
         request.get('http://localhost:6631/calculator/add/5/3',(err,rs,body)=>{
         expect(body).toBe('8')
+        done()
         })
     })
 })
